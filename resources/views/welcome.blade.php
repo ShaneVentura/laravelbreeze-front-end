da77243f409372fac07a38b80d476c71b5c25246
DonnaShane <donnashane.f.ventura@isu.edu.ph>
2023-12-18 23:05:31 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,128 @@
+<x-landing>
+    <x-slot name="header">
+        <div class=" bg-green-dark box-content flex items-center justify-center space-x-2 p-1">
+            <svg class="w-10 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
+                stroke="white">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            </svg>
+            <h1 class="text-white text-lg font-glacial font-medium ">
+                Smart energy management at your fingertips.
+            </h1>
+        </div>
+
+    </x-slot>
+
+    @include('layouts.navbar')
+
+
+    <div class="relative">
+        <div class="animate-updown absolute top-0 right-0 pr-96 mr-44 -mt-5">
+            <svg height="90px" width="90px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#1BC037"
+                stroke="#1BC037" stroke-width="10.24">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <g>
+                        <polygon style="fill:#ffffff;"
+                            points="357.016,284.718 414.093,251.548 184.458,478.831 127.381,512 ">
+                        </polygon>
+                        <polygon style="fill:#ffffff;"
+                            points="246.056,220.655 303.133,187.485 414.093,251.548 357.016,284.718 ">
+                        </polygon>
+                        <polygon style="fill:#ffffff;"
+                            points="327.203,104.979 384.28,71.81 303.133,187.485 246.056,220.655 ">
+                        </polygon>
+                        <polygon style="fill:#ffffff;" points="202.824,33.17 259.902,0 384.28,71.81 327.203,104.979 ">
+                        </polygon>
+                        <polygon style="fill:#ffffff;"
+                            points="357.016,284.718 127.381,512 218.95,282.398 97.907,212.514 202.824,33.17 327.203,104.979 246.056,220.655 ">
+                        </polygon>
+                    </g>
+                </g>
+            </svg>
+
+        </div>
+
+    </div>
+
+
+    <div class="content-center text-center mt-20">
+        <div>
+            <h1 class="text-blue-light font-extrabold text-7xl font-league tracking-normal">
+                The Smart Electric Meter
+            </h1>
+
+            <svg class="absolute bottom-0 left-0 animate-updown ml-40 mb-36" height="140px" width="140px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#1BC037"
+            stroke="#1BC037" stroke-width="10.24">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <g>
+                    <polygon style="fill:#ffffff;"
+                        points="357.016,284.718 414.093,251.548 184.458,478.831 127.381,512 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;"
+                        points="246.056,220.655 303.133,187.485 414.093,251.548 357.016,284.718 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;"
+                        points="327.203,104.979 384.28,71.81 303.133,187.485 246.056,220.655 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;" points="202.824,33.17 259.902,0 384.28,71.81 327.203,104.979 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;"
+                        points="357.016,284.718 127.381,512 218.95,282.398 97.907,212.514 202.824,33.17 327.203,104.979 246.056,220.655 ">
+                    </polygon>
+                </g>
+            </g>
+        </svg>
+
+            <h2 class="text-green-dark font-extrabold text-6xl font-league tracking-normal -mt-11">
+                for Power-saving companion
+            </h2>
+
+            <p class="text-black-light px-96 font-glacial font-normal text-2xl tracking-widest mt-2 opacity-70">
+                iKuryente is a web application designed for monitoring and controlling
+                purposes, offering a streamlined and intuitive interface for
+                effortless management.
+            </p>
+
+            <svg class="absolute bottom-0 right-0 animate-updown mr-56 mb-32" height="110px" width="110px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve" fill="#1BC037"
+            stroke="#1BC037" stroke-width="10.24">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <g>
+                    <polygon style="fill:#ffffff;"
+                        points="357.016,284.718 414.093,251.548 184.458,478.831 127.381,512 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;"
+                        points="246.056,220.655 303.133,187.485 414.093,251.548 357.016,284.718 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;"
+                        points="327.203,104.979 384.28,71.81 303.133,187.485 246.056,220.655 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;" points="202.824,33.17 259.902,0 384.28,71.81 327.203,104.979 ">
+                    </polygon>
+                    <polygon style="fill:#ffffff;"
+                        points="357.016,284.718 127.381,512 218.95,282.398 97.907,212.514 202.824,33.17 327.203,104.979 246.056,220.655 ">
+                    </polygon>
+                </g>
+            </g>
+        </svg>
+
+            <div class="mt-9">
+                <a href="http://localhost:8000/login"
+                    class="text-center font-boldglacial font-bold text-2xl tracking-wide items-center justify-center hover:opacity-80">
+                    <button class="text-white-light bg-green-dark rounded-sm py-1 px-24 hover:underline">
+                        Get Started
+                    </button>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</x-landing>
